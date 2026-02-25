71693e223f822f9d3f852a66dcedcc3157eb426c
hans <127169913+dxrkinfuser44@users.noreply.github.com>
2026-02-26 09:38:53 +1100

    base.py

---
+++ base.py
@@ -0,0 +1,30 @@
+# Import necessary libraries
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# ---------- STANDARD SETUP CODE ---------- #
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+#Initialise the sensors.
+#light_sensor = ColorSensor(Port.S1)
+#distance_sensor = UltrasonicSensor(Port.S2)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=152)
+
+# ---------- MAINLINE PROGRAM ---------- #
+
+### Here is where your code starts ###
+
+robot.drive(200, 0)
+wait(1000)
+robot.stop()
+wait(1000)
+robot.drive(-200, 0)
+wait(1000)
+robot.stop()
